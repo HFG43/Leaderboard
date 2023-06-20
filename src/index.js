9a1f3ac8af119840348d0c2510ea5ddef44656f8
HFG43 <117187653+HFG43@users.noreply.github.com>
2023-06-20 13:26:44 -0600

    Update Webhint Linter

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
-import _ from 'lodash';
 import './style.css';
+import Scores from './scores.js';
 
-function component() {
-  const element = document.createElement('div');
+const scores = [
+  {
+    player: 'Player1',
+    score: 100,
+    index: 0,
+  },
+  {
+    player: 'Player2',
+    score: 400,
+    index: 1,
+  },
+  {
+    player: 'Player3',
+    score: 200,
+    index: 2,
+  },
+  {
+    player: 'Player4',
+    score: 40,
+    index: 3,
+  },
+];
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+const scoreListContainer = document.querySelector('.recent_scores_list_container');
 
-  return element;
-}
-
-document.body.appendChild(component());
+const addscores = () => {
+  scores.forEach((score) => {
+    const newScore = new Scores(score.player, score.score, score.index);
+    const scoreContainer = document.createElement('li');
+    scoreContainer.innerText = `${newScore.player}: ${newScore.score}`;
+    scoreContainer.classList.add('score_item');
+    scoreListContainer.appendChild(scoreContainer);
+    return newScore;
+  });
+};
+addscores();
