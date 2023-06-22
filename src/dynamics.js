f805bcfd4f04a5e17b480eed2e0838fe1a29b28b
HFG43 <117187653+HFG43@users.noreply.github.com>
2023-06-21 21:33:35 -0600

    Add Get Scores function

--- src/dynamics.js
+++ src/dynamics.js
@@ -2,5 +2,7 @@ const form = document.querySelector('.new_score_form');
 const playerName = document.getElementById('name');
 const playerScore = document.getElementById('score');
 const submitNewScore = document.getElementById('submit');
+const refreshScores = document.getElementById('refresh');
+const scoreListContainer = document.querySelector('.recent_scores_list_container');
 
-export {submitNewScore, form, playerName, playerScore };
+export {submitNewScore, form, playerName, playerScore, refreshScores, scoreListContainer };
--- src/index.js
+++ src/index.js
@@ -1,46 +1,67 @@
 import './style.css';
 import Scores from './scores.js';
 import getGameId from './getGameId.js';
-import { submitNewScore, form, playerName, playerScore } from './dynamics.js';
+import {
+  submitNewScore,
+  form,
+  playerName,
+  playerScore,
+  refreshScores,
+  scoreListContainer,
+} from './dynamics.js';
 
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const newScore = new Scores("player1", 25);
-     setNewScore(newScore);
-});
+let scores = [];
 
 const setNewScore = async (newScore) => {
   const getId = await getGameId();
-  const result = await fetch(
-    `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${getId}/scores`,
-    {
-      method: 'POST',
-      body: JSON.stringify(newScore),
-      headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-      },
-    },
-  );
-
-  if (result.ok) {
-      const nameResult = await result.json();
-      console.log(nameResult)
-  } else {
-    console.log('Define a new name');
+  try {
+    const result = await fetch(
+      `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${getId}/scores`,
+      {
+        method: 'POST',
+        body: JSON.stringify(newScore),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      }
+    );
+    const newScoreResult = await result.json();
+    return newScoreResult;
+  } catch (error) {
+    return error;
   }
 };
 
+const getScores = async () => {
+  const getId = await getGameId();
+  try {
+    const resultSaved = await fetch(
+      `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${getId}/scores`
+    );
+    const savedScores = await resultSaved.json();
+    console.log(savedScores);
+  } catch (error) {
+    return error;
+  }
+};
+getScores();
 
-// const scoreListContainer = document.querySelector('.recent_scores_list_container');
+const addscores = async () => {
+  scores.forEach((score) => {
+    scores = [];
+    const scoreContainer = document.createElement('li');
+    scoreContainer.innerText = `${score.user}: ${score.score}`;
+    scoreContainer.classList.add('score_item');
+    scoreListContainer.appendChild(scoreContainer);
+  });
+};
 
-// const addscores = () => {
-//   scores.forEach((score) => {
-//     const newScore = new Scores(score.user, score.score);
-//     const scoreContainer = document.createElement('li');
-//     scoreContainer.innerText = `${newScore.user}: ${newScore.score}`;
-//     scoreContainer.classList.add('score_item');
-//     scoreListContainer.appendChild(scoreContainer);
-//     return newScore;
-//   });
-// };
-// addscores();
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const name = playerName.value;
+  const score = playerScore.value;
+  const newScore = new Scores(name, score);
+  scores = [...scores, newScore];
+  setNewScore(newScore);
+  addscores();
+});
